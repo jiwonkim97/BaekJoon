f56e83ae212951cbacc20218fb599ed285165809
jiwonkim97 <jiwonkim97@naver.com>
2022-01-04 23:22:49 +0900

    Create 1550.js

---
+++ code/JS/1550.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');   // input은 줄 단위로 끊어진 배열로 저장 및 string임.
+
+// let input = ['A'];
+
+console.log(parseInt(input[0], 16));
