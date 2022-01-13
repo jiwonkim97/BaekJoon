ef965b09791bac60c90a2ec1b95d230bf6fdbfd1
jiwonkim97 <jiwonkim97@naver.com>
2022-01-13 16:06:33 +0900

    Update 1018.js

--- code/JS/1018.js
+++ code/JS/1018.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-// let input = ['8 8', 'WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBBBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW'];
+let input = ['8 8', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBBBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW'];
 // let input = ['10 13',
 // 'BBBBBBBBWBWBW',
 // 'BBBBBBBBBWBWB',
@@ -16,40 +16,40 @@ let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const CHESSBOARD = [
     [
-        ['B','W','B','W','B','W','B','W'],
-        ['W','B','W','B','W','B','W','B'],
-        ['B','W','B','W','B','W','B','W'],
-        ['W','B','W','B','W','B','W','B'],
-        ['B','W','B','W','B','W','B','W'],
-        ['W','B','W','B','W','B','W','B'],
-        ['B','W','B','W','B','W','B','W'],
-        ['W','B','W','B','W','B','W','B']
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
     ],
     [
-        ['W','B','W','B','W','B','W','B'],
-        ['B','W','B','W','B','W','B','W'],
-        ['W','B','W','B','W','B','W','B'],
-        ['B','W','B','W','B','W','B','W'],
-        ['W','B','W','B','W','B','W','B'],
-        ['B','W','B','W','B','W','B','W'],
-        ['W','B','W','B','W','B','W','B'],
-        ['B','W','B','W','B','W','B','W']
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
     ]
-    ];
-const [ M, N ] = input[0].split(' ');
-const board = input.slice(1,M + 1).map(c => c.split(''))
+];
+const [M, N] = input.shift().split(' ');
+const board = input.slice(0, M).map(c => c.split(''))
 let arr1 = [];
 let arr2 = [];
 
-for(let i = 0 ; i <= M-8 ; i ++){
-    for(let j = 0 ; j <= N-8 ; j ++){
+for (let i = 0; i <= M - 8; i++) {
+    for (let j = 0; j <= N - 8; j++) {
         let diff1 = 0;
         let diff2 = 0;
-        for(let n = 0 ; n < 8 ; n ++){
-            for(let m = 0 ; m < 8 ; m ++){
-                if(board[i+n][j+m] !== CHESSBOARD[0][n][m])
+        for (let n = 0; n < 8; n++) {
+            for (let m = 0; m < 8; m++) {
+                if (board[i + n][j + m] !== CHESSBOARD[0][n][m])
                     diff1++;
-                if(board[i+n][j+m] !== CHESSBOARD[1][n][m])
+                if (board[i + n][j + m] !== CHESSBOARD[1][n][m])
                     diff2++;
             }
         }
