0f2ce3f3e17ba59fc1923d2a83911590eba896fd
jiwonkim97 <jiwonkim97@naver.com>
2022-01-14 17:42:26 +0900

    Update 1662.js

--- code/JS/backlog/1662.js
+++ code/JS/backlog/1662.js
@@ -1,34 +1,43 @@
-const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');   // input은 줄 단위로 끊어진 배열로 저장 및 string임.
+// const fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');   // input은 줄 단위로 끊어진 배열로 저장 및 string임.
 
-// let input = ['33(562(71(9)))'];  //19
+let input = ['33(562(71(9)))'];  //19
 // let input = ['123']; //3
 // let input = ['10342(76)'];  //8
 // let input = ['0(0)'];  //0
 // let input = ['1(1(1(1(1(1(1(0(1234567890))))))))'];  //0
 // let input = ['1()66(5)'];  //7
 // let input = ['22(2)22(2)22(2)'];
+// let input = ['2(3)4'];
 
 const unzip = (c) => {
-  // console.log(c);
+  console.log(c);
   let stack = [];
+  let ret = 0;
   for (let i = 0; i < c.length; i++) {
     if (c[i] === ')') {
       // console.log(stack);
       let arr = [];
+      let cnt = 0;
       while (stack[stack.length - 1] !== '(') {
         arr.push(stack.pop());
       }
       stack.pop();  // '(' 빼주기
+      let temp = stack.pop();
+      while (stack.length) {
+        stack.pop();
+        cnt++;
+      }
       // console.log('str: ' + str)
-      stack.push(unzip(arr.reverse().join('')).repeat(stack.pop()));
+      // stack.push(unzip(arr.reverse().join('')).repeat(stack.pop()));
+      ret = cnt + temp * unzip(arr.reverse().join(''));
     }
     else {
       stack.push(c[i]);
     }
   }
-  return stack.join('');
+  return ret ? ret : stack.length;
 };
 
 // console.log(unzip(input[0]));
-console.log(unzip(input[0]).length);
+console.log(unzip(input[0]));
